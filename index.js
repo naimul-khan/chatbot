27286d03bd7f533758f93246cb9c5b38fc5def4c
nk155742 <nk155742@BC.Baruch.Cuny.Edu>
2017-08-29 19:41:06 -0400

    added facebook compat

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(bodyParser.json()) // parse text into JSON
 app.get('/', function(req, res) {
     res.send("Hi, I am a chatbot")
 })
+
+token = "EAADujaO0AlkBAJYd5Si2aWeDFrV2yVRhs7AgZAWoV4UpBLJQsO1EhBMi4fyBnJqZCxQvbZBve6KTiy9ZB2risnVXdR5TUO0fiUOGPkdfeMNtOoQhYwcY92t1z1ioEhq35cFRh03oZC8Tc7IuE5ZAt0xDTWJEhpIrWUUeF0Mak2hgZDZD"
 // Facebook, security thing
 app.get('/webhook/', function(req, res){ 
     if (req.query['hub.verify_token'] === "nk1493") { 
@@ -27,6 +29,38 @@ app.get('/webhook/', function(req, res){
     }
     res.send("Wrong token")
 })
+
+app.post('/webhook/', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging_events
+    for (let i=0; i < messaging_events.length; i++){ 
+    let event = messaging_events[i]
+    let sender = event.sender.id
+    if (event.message && event.message.text) { 
+          let text = event.message.text 
+          sendText(sender, "Text echo: " + text.substring(0,100))
+    }
+    }
+    res.sendStatus(200)
+})
+
+function sendText(sender, text){ 
+    let messageData = {text: text}
+    request({ 
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token, token},
+        method: "POST",
+        json: { 
+            receipt: {id, sender}, 
+            message: messageData
+    }, function(error, response, body){ 
+    if(error){ 
+        console.log("sending error")
+    } else if (response.body.error){ 
+        console.log("response body error")
+    }
+    }
+    })
+}
 
 // start the server
 app.listen(app.get('port'), function(){
